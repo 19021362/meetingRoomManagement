475a3ad2ea401c341d6e6479de73677980905b29
19021362 <19021362@vnu.edu.vn>
2021-10-31 18:31:58 +0700

    Update App.js

--- meeting-room-management-app/src/App.js
+++ meeting-room-management-app/src/App.js
@@ -4,19 +4,22 @@ import Routes from './Route.js';
 import './styles/App.css';
 import { LinkContainer } from "react-router-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import { Navbar } from 'react-bootstrap';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App container py-3">
-      <Header />
-      
-      <Routes />
+    <Router>
+      <div className="App container py-3">
+        <Header />
 
-      <Footer />
-    </div>
+        <Routes />
+
+        <Footer />
+      </div>
+    </Router>
+
   );
 };
 
